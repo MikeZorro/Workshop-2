bd68d943064d888e4130d8c884de7fd06aea842e
Michal Soroczynski <soroczynski.m@gmail.com>
2022-05-13 10:45:44 +0200

    starting to work on DAOcreate method

--- src/UserDAO/Tests.java
+++ src/UserDAO/Tests.java
@@ -15,4 +15,6 @@ public class Tests {
             e.printStackTrace();
         }
     }
+
+
 }
--- src/UserDAO/UserDAO.java
+++ src/UserDAO/UserDAO.java
@@ -1,4 +1,18 @@
 package UserDAO;
 
+import org.mindrot.jbcrypt.BCrypt;
+import java.sql.*;
+import java.sql.PreparedStatement;
+
 public class UserDAO {
+    private static final String CREATE_USER_QUERY =
+            "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
+
+    public String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+
+
+
 }
